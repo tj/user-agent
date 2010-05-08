7d52a6487fad6426c86068a92a9d195affa50658
Douglas Meyer <douglas.meyer@centro.net>
2010-05-08 14:35:34 -0500

    Adding support for firefox.

--- lib/user-agent/agent.rb
+++ lib/user-agent/agent.rb
@@ -1,92 +1,92 @@
 
 class Agent
-  
+
   ##
   # User agent string.
-  
+
   attr_reader :string
-  
+
   ##
   # Initialize with user agent _string_.
-  
+
   def initialize string
     @string = string.strip
   end
-  
+
   #--
   # Instance methods
   #++
-  
+
   ##
   # User agent name symbol.
-  
+
   def name
     Agent.name_for_user_agent string
   end
-  
+
   ##
   # User agent version.
-  
+
   def version
     Agent.version_for_user_agent string
   end
-  
+
   ##
   # User agent engine symbol.
-  
+
   def engine
     Agent.engine_for_user_agent string
   end
-  
+
   ##
   # User agent engine version string.
-  
+
   def engine_version
     Agent.engine_version_for_user_agent string
   end
-  
+
   ##
   # User agent os symbol.
-  
+
   def os
     Agent.os_for_user_agent string
   end
-  
+
   ##
   # User agent string.
-  
+
   def to_s
     string
   end
-  
+
   ##
   # Inspect.
-  
+
   def inspect
     "#<Agent:#{name} version:#{version.inspect} engine:\"#{engine.to_s}:#{engine_version}\" os:#{os.to_s.inspect}>"
   end
-  
+
   ##
   # Check if the agent is the same as _other_ agent.
-  
+
   def == other
     string == other.string
   end
-  
+
   #--
   # Class methods
   #++
-  
+
   ##
   # Return engine version for user agent _string_.
-  
+
   def self.engine_version_for_user_agent string
     $1 if string =~ /#{engine_for_user_agent(string)}[\/ ]([\d\w\.\-]+)/i
   end
-  
+
   ##
   # Return version for user agent _string_.
-  
+
   def self.version_for_user_agent string
     case name = name_for_user_agent(string)
     when :Chrome ; $1 if string =~ /chrome\/([\d\w\.\-]+)/i
@@ -96,10 +96,10 @@ class Agent
     else           $1 if string =~ /#{name}[\/ ]([\d\w\.\-]+)/i
     end
   end
-  
+
   ##
   # Return engine symbol for user agent _string_.
-  
+
   def self.engine_for_user_agent string
     case string
     when /webkit/i    ; :webkit
@@ -112,10 +112,10 @@ class Agent
     else                :unknown
     end
   end
-  
+
   ##
   # Return the os for user agent _string_.
-  
+
   def self.os_for_user_agent string
     case string
     when /windows nt 6\.0/i      ; :'Windows Vista'
@@ -131,10 +131,10 @@ class Agent
     else                         ; :Unknown
     end
   end
-  
+
   ##
   # Return name for user agent _string_.
-  
+
   def self.name_for_user_agent string
     case string
     when /konqueror/i            ; :Konqueror
@@ -144,15 +144,16 @@ class Agent
     when /opera/i                ; :Opera
     when /playstation 3/i        ; :PS3
     when /playstation portable/i ; :PSP
+    when /firefox/i              ; :Firefox
     else                         ; :Unknown
     end
   end
-  
+
   @agents = []
-  
+
   ##
   # Map agent _name_ to _options_.
-  
+
   def self.map name, options = {}
     @agents << [name, options]
   end
